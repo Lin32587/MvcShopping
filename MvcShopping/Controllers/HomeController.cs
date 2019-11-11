4c2be3b285177ab1825a6ba45e588d0da0514215
Lin <lin3258714569@gmail.com>
2019-11-11 09:28:04 +0800

    new

--- MvcShopping/Controllers/HomeController.cs
+++ MvcShopping/Controllers/HomeController.cs
@@ -3,27 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Drawing;
+using MvcShopping.Controllers;
+using MvcShopping.Models;
 
 namespace MvcShopping.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
+        //首页
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        //商品列表
+        public ActionResult ProductList(int Id)
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        //商品明细
+        public ActionResult ProductDetail(int Id)
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
