70f054117aa5bbcf82059423cdee4eec610a6ccc
Lin <lin3258714569@gmail.com>
2019-11-11 10:25:00 +0800

    controllers

---
+++ MvcShopping/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcShopping.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        //显示当前购物车的项目
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //添加产品项目到购物车,如果没有传入Amount参数则默认购买数量为1
+        [HttpPost]
+        public ActionResult AddToCart(int ProductId,int Amount = 1)
+        {
+            return View();
+        }
+
+        //移除购物车项目
+        [HttpPost]
+        public ActionResult Remove(int ProductId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateAmount(int ProductId,int NewAmount)
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcShopping/Controllers/MemberController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcShopping.Models;
+using System.Web.Security;
+
+namespace MvcShopping.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        //会员注册页面
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //写入会员信息
+        [HttpPost]
+        public ActionResult Register(Members member)
+        {
+            return View();
+        }
+
+        //显示会员登录页面
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //运行会员登录
+        [HttpPost]
+        public ActionResult Login(string email,string password,string returnUrl)
+        {
+            if (ValidateUser(email, password))
+            {
+                FormsAuthentication.SetAuthCookie(email, false);
+
+                if (String.IsNullOrEmpty(returnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return Redirect(returnUrl);
+                }
+            }
+            ModelState.AddModelError("", "您输入的账号或密码错误");
+            return View();
+        }
+
+        public bool ValidateUser(string email,string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Logout()
+        {
+            //清除Cookies
+            FormsAuthentication.SignOut();
+
+            //清除Session
+            Session.Clear();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
