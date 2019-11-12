c51f0ffe4e942ffed2658f36ee7baf72c79bc455
Lin <lin3258714569@gmail.com>
2019-11-12 17:50:10 +0800

    NewView

--- MvcShopping/Controllers/CartController.cs
+++ MvcShopping/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcShopping.Models;
 
 namespace MvcShopping.Controllers
 {
@@ -12,7 +13,13 @@ namespace MvcShopping.Controllers
         //显示当前购物车的项目
         public ActionResult Index()
         {
-            return View();
+            var productCate = new ProductCategory() { Id = 1, Name = "文具" };
+            var a = new Product() { Id = 3, Price = 6, Name = "铅笔", ProductCategory = productCate, Color = System.Drawing.Color.White, Description = "", PublishOn = DateTime.Now };
+            var data = new List<Cart>
+            {
+                new Cart(){ Product=a,Amount=10}
+            };
+            return View(data);
         }
 
         //添加产品项目到购物车,如果没有传入Amount参数则默认购买数量为1
